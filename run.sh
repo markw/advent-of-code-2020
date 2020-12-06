d826ed93d550e42e5e1672c839c8e15b1fb1ab69
Mark Williams <mwilliams@cj.com>
2020-12-06 09:54:13 -0800

    tweak script

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in $(ls -1 *.java | sort); do
+for file in $(ls -1 Day*.java | sort); do
     echo ----- $file -----
     javac $file 
     java $(echo $file | sed -e 's/.java//')
